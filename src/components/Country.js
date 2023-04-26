83d3923c6d28a1d6f3768c3c8fdb047fa51b67a3
Besufekad Alemu <besurye@gmail.com>
2023-04-26 23:38:10 +0300

    Fix eslint errors

--- src/components/Country.js
+++ src/components/Country.js
@@ -10,60 +10,59 @@ import '../stylesheet/Country.css';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const POLLUTANTS = ['CO', 'NH3', 'NO', 'NO2', 'O3', 'PM2_5', 'PM10', 'SO2'];
-
-const chartData = (infoArr) => {
-  const data = POLLUTANTS.map((pollutant) => infoArr[pollutant.toLowerCase()]);
-  const backgroundColors = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-    'rgba(94, 94, 94, 0.2)',
-    'rgba(0, 0, 0, 0.2)',
-  ];
-  const borderColors = [
-    'rgba(255, 99, 132, 1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-    'rgba(94, 94, 94, 1)',
-    'rgba(0, 0, 0, 1)',
-  ];
-
-  return {
-    labels: POLLUTANTS,
-    datasets: [
-      {
-        label: 'Quantity',
-        data,
-        backgroundColor: backgroundColors,
-        borderColor: borderColors,
-        borderWidth: 1,
-      },
-    ],
-  };
-};
-
-const ChartRadar = ({ data }) => <Doughnut data={data} />;
-
 const Country = () => {
   const {
     info, lat, lon, name,
   } = useParams();
   const infoArr = useSelector((state) => state.info);
   const dispatch = useDispatch();
-
   useEffect(() => {
     const coor = [lat, lon];
     dispatch(getInfo(coor));
   }, [dispatch, lat, lon]);
+  const data = {
+    labels: ['CO', 'NH3', 'NO', 'NO2', 'O3', 'PM2_5', 'PM10', 'SO2'],
+    datasets: [
+      {
+        label: 'Quantity',
+        data: [
+          infoArr.co,
+          infoArr.nh3,
+          infoArr.no,
+          infoArr.no2,
+          infoArr.o3,
+          infoArr.pm2_5,
+          infoArr.pm10,
+          infoArr.so2,
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(94, 94, 94, 0.2)',
+          'rgba(0, 0, 0, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(94, 94, 94, 1)',
+          'rgba(0, 0, 0, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-  const data = chartData(infoArr);
+  function ChartRadar() {
+    return <Doughnut data={data} />;
+  }
 
   return (
     <div className="country">
@@ -84,15 +83,18 @@ const Country = () => {
         <h3>{name}</h3>
         <div className="infoPollution">
           <h4>Air Pollution</h4>
-          {POLLUTANTS.map((pollutant) => (
-            <p key={pollutant}>
-              {`${pollutant}: ${infoArr[pollutant.toLowerCase()]}`}
-            </p>
-          ))}
+          <p>{`CO: ${infoArr.co}`}</p>
+          <p>{`NH3: ${infoArr.nh3}`}</p>
+          <p>{`NO: ${infoArr.no}`}</p>
+          <p>{`NO2: ${infoArr.no2}`}</p>
+          <p>{`O3: ${infoArr.o3}`}</p>
+          <p>{`PM2_5: ${infoArr.pm2_5}`}</p>
+          <p>{`PM10: ${infoArr.pm10}`}</p>
+          <p>{`SO2: ${infoArr.so2}`}</p>
         </div>
         <div className="chart">
           <h2>Doughnut Chart</h2>
-          <ChartRadar data={data} />
+          <ChartRadar />
         </div>
       </div>
     </div>
